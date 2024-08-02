9b74ac39c510bd9cdeff79d08e652a3db2981d56
thezaidbintariq <ztariq.bscs19seecs@seecs.edu.pk>
2024-08-02 17:28:45 -0400

    added DPX controller

--- api/app/Http/Controllers/DPX.php
+++ api/app/Http/Controllers/DPX.php
@@ -6,271 +6,229 @@ use App\Http\Controllers\API;
 use App\Models\Wallet;
 use App\Models\Transaction;
 use Illuminate\Support\Facades\Hash;
-
+use Web3\Web3;
+use GuzzleHttp\Client;
+use Tron\Address;
+use Carbon\Carbon;
+// use Tron\Api;
+use Tron\TRX;
 class DPX extends Controller
 {
-    
-    public static function CreateWallet(null|string $wallet = null) {
+    const URI = 'https://nile.trongrid.io';
+    public static function GenerateTRXAddress()
+    {
+        try {
+            $api = new \Tron\Api(new Client(['base_uri' => self::URI]));
+            $trxWallet = new \Tron\TRX($api);
+            $addressData = $trxWallet->generateAddress();
+            // $addressData->privateKey
+            // $addressData->address
 
-        if (!($wallet)) {
-
-            $wallet = DPX::GenerateRandomHash();
-
-        }
-
-        $secret = DPX::GenerateRandomHash();
-
-        Wallet::insert([ 'wallet' => $wallet, 'secret' => Hash::make($secret) ]);
-
-        return [ 'wallet' => $wallet, 'secret' => $secret ];
-
-    }
-
-    public static function Transfer(string $departure, string $destination, float $amount, string $secret, float $fee = null) {
-
-        if (!(preg_match('/' . config('regex.md5') . '/', $departure))) {
-
-            return API::Error('invalid-departure', 'Departure wallet is invalid');
-
-        }
-
-        if (!(preg_match('/' . config('regex.md5') . '/', $destination))) {
-
-            return API::Error('invalid-destination', 'Destination wallet is invalid');
-
-        }
-
-        if (!(preg_match('/' . config('regex.md5') . '/', $secret))) {
-
-            return API::Error('invalid-secret-format', 'Secret phrase format is invalid');
-
-        }
-
-        if (!(preg_match('/' . config('regex.amount') . '/', $amount))) {
-
-            return API::Error('invalid-amount', 'Amount is invalid');
-
-        }
-
-        if ($departure === $destination) {
-
-            return API::Error('same-departure-destination', 'Departure and Destination are same');
-
-        }
-
-        $amount = round($amount, 1);
-
-        if ($amount < env('FEE', 0.1)) {
-
-            return API::Error('invalid-amount', 'Entered value is less than its fee');
-
-        }
-
-        $data = Wallet::where([ 'wallet' => $departure ])->orwhere([ 'wallet' => $destination ])->get()->toArray();
-
-        foreach ($data as $key => $value) {
-
-            $data[$value['wallet']] = json_decode(json_encode($value), true);
-            unset($data[$key]);
-
-        }
-
-        foreach ([$departure, $destination] as $wallet) {
-
-            if (isset($data[$wallet])) {
-
-                if ((bool)$data[$wallet]['locked']) {
-
-                    return API::Error('locked-' . ($wallet === $departure ? 'departure' : 'destination') . '-wallet', 'Departure/Destination wallet is locked');
-
-                }
-
-            }else{
-                
-                return API::Error('invalid-wallet', 'Wallet is invalid');
-
-            }
-
-        }
-
-        if (!(Hash::check($secret, $data[$departure]['secret']))) {
-
-            return API::Error('invalid-secret', 'Secret is invalid');
-
-        }
-
-        $destinationtalBalance = $data[$departure]['balance'] + $data[$departure]['bonus'];
-
-        if (is_null($fee)) {
-
-            $fee = env('FEE', 0.1);
-            
-        }
-
-        if ($destinationtalBalance < ($amount + $fee)) {
-
-            return API::Error('insufficient-balance', 'Balance is not sufficent');
-
-        }
-
-        if ($fee > 0) {
-
-            if (preg_match('/' . config('regex.md5') . '/', (string)env('FEE_WALLET', ''))) {
-                
-                DPX::Transfer($departure, env('FEE_WALLET'), $fee, $secret, 0);
-
-            }
-
-            $data = Wallet::where([ 'wallet' => $departure ])->orwhere([ 'wallet' => $destination ])->get()->toArray();
-
-            foreach ($data as $key => $value) {
-    
-                $data[$value['wallet']] = json_decode(json_encode($value), true);
-                unset($data[$key]);
-    
-            }
-
-            $transaction = [
-                'transaction' => DPX::GenerateRandomHash(),
-                'departure'   => $departure,
-                'destination' => $destination,
-                'amount'      => $amount,
-                'fee'         => $fee,
-                'timestamp'   => time(),
+            $config = [
+                'contract_address' => 'TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t', // USDT TRC20
+                'decimals' => 6,
             ];
-    
-            Transaction::insert($transaction);
+            $trc20Wallet = new \Tron\TRC20($api, $config);
+            $addressData = $trc20Wallet->generateAddress();
 
+        } catch (\Exception $e) {
+            return null;
         }
-
-        $bonusRemaining   = $data[$departure]['bonus'] - $amount;
-        $balanceRemaining = $data[$departure]['balance'];
-
-        if ($bonusRemaining < 0) {
-
-            $balanceRemaining -= abs($bonusRemaining);
-
-        }
-        
-        Wallet::where([ 'wallet' => $departure ])->update([ 
-            'bonus' => max(0, $bonusRemaining),
-            'balance' => $balanceRemaining
-        ]);
-
-        Wallet::where([ 'wallet' => $destination ])->update([
-            'balance' => $data[$destination]['balance'] + $amount
-        ]);
-
-        return API::Respond($transaction ?? []);
-
+        return ['addressData' => $addressData];
     }
 
-    public static function Verify(string $wallet, string $secret) {
+    public static function ValidateTRXAddress(string $address, string $privateKey, string $hexAddress)
+    {
+        try {
+            $api = new \Tron\Api(new Client(['base_uri' => self::URI]));
+            $trxWallet = new \Tron\TRX($api);
+            $tronAddress = new \Tron\Address($address, $privateKey, $hexAddress);
+            $addressData = $trxWallet->validateAddress(
+                $tronAddress
+            );
 
-        $wallet = Wallet::where('wallet', $wallet)->first();
+        } catch (\Exception $e) {
+            return ['addressData' => false];
+        }
 
-        if ($wallet) {
+        return ['addressData' => $addressData];
+    }
 
-            return Hash::check($secret, $wallet->secret);
 
+    public static function CreateWallet(null|string $wallet = null)
+    {
+    // Generate the wallet if not provided
+    if (!($wallet)) {
+        $wallet = DPX::GenerateTRXAddress();
+    }
+    // dd($wallet['addressData']->privateKey);
+
+    // Access the 'addressData' object within the array
+    if (isset($wallet['addressData']) && is_object($wallet['addressData'])) {
+
+        $addressData = $wallet['addressData'];
+        // Access the properties directly
+        $address = $addressData->address;
+        $privateKey = $addressData->privateKey;
+
+        // Insert the wallet into the Wallet table
+        Wallet::insert([
+            'wallet' => $address,
+            'secret' => Hash::make($privateKey),
+            'hexAddress' => $addressData->hexAddress
+        ]);
+
+        // Return the address and private key
+        return [
+            'wallet' => $address,
+            'secret' => $privateKey,
+            'hexAddress' => $addressData->hexAddress
+        ];
+    } else {
+        // Handle the case where addressData is not set or not an object
+        throw new \Exception('Address data is missing or not an object.');
+    }
+}
+
+
+    public static function Transfer(string $departure, string $destination, float $amount, string $secret, float $fee = null)
+    {
+        try {
+            $api = new \Tron\Api(new Client(['base_uri' => self::URI]));
+            $trxWallet = new \Tron\TRX($api);
+
+            $from = $trxWallet->privateKeyToAddress($secret);
+            $to = new Address(
+                $destination,
+                '',
+                $trxWallet->tron->address2HexString($destination)
+            );
+            $transferData = $trxWallet->transfer($from, $to, $amount);
+
+            $responseData = [
+                "transaction" => $transferData->txID,
+                "departure" => $departure,
+                "destination" => $destination,
+                "amount" => $amount,
+                "fee" => $fee ?? "0.2", // Set a default fee if not provided
+                "timestamp" => Carbon::createFromTimestampMs($transferData->raw_data['timestamp'])->toDateTimeString(), // Convert to seconds
+            ];
+
+            Transaction::insert($responseData);
+
+        } catch (\Exception $e) {
+            return API::Error('error', $e->getMessage());
+        }
+
+        return API::Respond($responseData ?? []);
+    }
+
+    public static function Verify(string $wallet, string $secret, string $hexAddress)
+    {
+
+        $validatedAddress = DPX::ValidateTRXAddress($wallet, $secret, $hexAddress);
+
+        if ($validatedAddress['addressData'] === true) {
+
+            return true;
         }
 
         return false;
-
     }
 
-    public static function RevokeSecret(string $wallet, string $secret) {
+    // public static function RevokeSecret(string $wallet, string $secret)
+    // {
 
-        $wallet = Wallet::where('wallet', $wallet)->first();
+    //     $wallet = Wallet::where('wallet', $wallet)->first();
 
-        if ($wallet && Hash::check($secret, $wallet->secret)) {
+    //     if ($wallet && Hash::check($secret, $wallet->secret)) {
 
-            $new_secret = DPX::GenerateRandomHash();
+    //         $new_secret = DPX::GenerateRandomHash();
 
-            Wallet::where([ 'wallet' => $wallet ])->update([ 'secret' => Hash::make($new_secret) ]);
+    //         Wallet::where(['wallet' => $wallet])->update(['secret' => Hash::make($new_secret)]);
 
-            return $new_secret;
+    //         return $new_secret;
+    //     }
 
-        }
+    //     return false;
+    // }
 
-        return false;
 
+    public static function GetBalance(string $wallet)
+    {
+        $api = new \Tron\Api(new Client(['base_uri' => self::URI]));
+        $trxWallet = new \Tron\TRX($api);
+
+        $address = new Address(
+            $wallet,
+            '',
+            $trxWallet->tron->address2HexString($wallet)
+        );
+
+        $balanceData = $trxWallet->balance($address);
+
+        return $balanceData ? API::Respond($balanceData) : API::Error('invalid-wallet', 'Wallet is invalid');
     }
 
-    public static function GetBalance(string $wallet) {
+    public static function GetTransaction(string $transaction)
+    {
 
-        $balance = Wallet::where([ 'wallet' => $wallet ])->first([ 'balance' ])->balance ?? null;
+        $transactionInfo = Transaction::where(['transaction' => $transaction])->first(['transaction', 'departure', 'destination', 'amount', 'fee', 'timestamp']);
 
-        return $balance ? API::Respond($balance) : API::Error('invalid-wallet', 'Wallet is invalid');
-
+        return $transactionInfo ? API::Respond($transactionInfo) : API::Error('invalid-transaction', 'Transaction is invalid');
     }
 
-    public static function GetTransaction(string $transaction) {
-
-        $transaction = Transaction::where([ 'transaction' => $transaction ])->first([ 'transaction', 'departure', 'destination', 'amount', 'fee', 'timestamp' ]);
-
-        return $transaction ? API::Respond($transaction) : API::Error('invalid-transaction', 'Transaction is invalid');
-
-    }
-
-    public static function GetTransactions(int $offset = 0, string|null $departure = null, string|null $destination = null) {
+    public static function GetTransactions(int $offset = 0, string|null $departure = null, string|null $destination = null)
+    {
 
         if ($departure && $destination) {
 
             if ($departure === $destination) {
 
-                $transactions = Transaction::where([ 'departure' => $departure ])
-                                ->orWhere([ 'destination' => $destination ])
-                                ->orderby('id', 'DESC')
-                                ->limit(env('TRANSACTIONS_PER_FETCH', 250))
-                                ->offset($offset)
-                                ->get([ 'transaction', 'departure', 'destination', 'amount', 'fee', 'timestamp' ]);
+                $transactions = Transaction::where(['departure' => $departure])
+                    ->orWhere(['destination' => $destination])
+                    ->orderby('id', 'DESC')
+                    ->limit(env('TRANSACTIONS_PER_FETCH', 250))
+                    ->offset($offset)
+                    ->get(['transaction', 'departure', 'destination', 'amount', 'fee', 'timestamp']);
+            } else {
 
-            }else{
-                
-                $transactions = Transaction::where([ 'departure' => $departure, 'destination' => $destination ])
-                                ->orderby('id', 'DESC')
-                                ->limit(env('TRANSACTIONS_PER_FETCH', 250))
-                                ->offset($offset)
-                                ->get([ 'transaction', 'departure', 'destination', 'amount', 'fee', 'timestamp' ]);
-
+                $transactions = Transaction::where(['departure' => $departure, 'destination' => $destination])
+                    ->orderby('id', 'DESC')
+                    ->limit(env('TRANSACTIONS_PER_FETCH', 250))
+                    ->offset($offset)
+                    ->get(['transaction', 'departure', 'destination', 'amount', 'fee', 'timestamp']);
             }
+        } else if ($departure) {
 
-        }else if ($departure) {
+            $transactions = Transaction::where(['departure' => $departure])
+                ->orderby('id', 'DESC')
+                ->limit(env('TRANSACTIONS_PER_FETCH', 250))
+                ->offset($offset)
+                ->get(['transaction', 'departure', 'destination', 'amount', 'fee', 'timestamp']);
+        } else if ($destination) {
 
-            $transactions = Transaction::where([ 'departure' => $departure ])
-                            ->orderby('id', 'DESC')
-                            ->limit(env('TRANSACTIONS_PER_FETCH', 250))
-                            ->offset($offset)
-                            ->get([ 'transaction', 'departure', 'destination', 'amount', 'fee', 'timestamp' ]);
-
-        }else if ($destination) {
-
-            $transactions = Transaction::where([ 'destination' => $destination ])
-                            ->orderby('id', 'DESC')
-                            ->limit(env('TRANSACTIONS_PER_FETCH', 250))
-                            ->offset($offset)
-                            ->get([ 'transaction', 'departure', 'destination', 'amount', 'fee', 'timestamp' ]);
-
-        }else{
+            $transactions = Transaction::where(['destination' => $destination])
+                ->orderby('id', 'DESC')
+                ->limit(env('TRANSACTIONS_PER_FETCH', 250))
+                ->offset($offset)
+                ->get(['transaction', 'departure', 'destination', 'amount', 'fee', 'timestamp']);
+        } else {
 
             $transactions = Transaction::orderby('id', 'DESC')
-                            ->limit(env('TRANSACTIONS_PER_FETCH', 250))
-                            ->offset($offset)
-                            ->get([ 'transaction', 'departure', 'destination', 'amount', 'fee', 'timestamp' ]);
-
+                ->limit(env('TRANSACTIONS_PER_FETCH', 250))
+                ->offset($offset)
+                ->get(['transaction', 'departure', 'destination', 'amount', 'fee', 'timestamp']);
         }
 
         $transactions = json_decode(json_encode($transactions), true);
 
         return API::Respond($transactions, json_encode: false);
-
     }
 
-    public static function GenerateRandomHash() {
+    // public static function GenerateRandomHash()
+    // {
 
-        return md5(random_bytes(32) . microtime(true));
-
-    }
-
+    //     return md5(random_bytes(32) . microtime(true));
+    // }
 }
