d87f4ca944da2c2591414279f765f4ea5cbdd6dd
thezaidbintariq <ztariq.bscs19seecs@seecs.edu.pk>
2024-08-02 17:24:22 -0400

    api copied

--- api/routes/api.php
+++ api/routes/api.php
@@ -3,11 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API;
 
-Route::get('/balance/{wallet}', [ API::class, 'Balance'      ])->where([ 'wallet' => config('regex.md5') ]);
-Route::get('/transaction/{id}', [ API::class, 'Transaction'  ])->where([ 'id' => config('regex.md5') ]);
+Route::get('/balance/{wallet}', [ API::class, 'Balance'      ]); // complete
+Route::get('/transaction/{id}', [ API::class, 'Transaction'  ]); // complete
 Route::any('/transactions',     [ API::class, 'Transactions' ]);
-Route::post('/transfer',        [ API::class, 'Transfer'     ]);
-Route::post('/verify',          [ API::class, 'Verify'       ])->middleware('throttle:6,1');
-Route::post('/revoke',          [ API::class, 'Revoke'       ])->middleware('throttle:6,1');
-Route::post('/generate',        [ API::class, 'Generate'     ])->middleware('throttle:6,1');
-Route::get('/{any}',            [ API::class, 'Invalid'      ])->where([ 'any' => '.*' ]);
+Route::post('/transfer',        [ API::class, 'Transfer'     ]); // complete
+Route::post('/verify',          [ API::class, 'Verify'       ]); // complete
+// Route::post('/revoke',          [ API::class, 'Revoke'       ]); // not needed
+Route::post('/generate',        [ API::class, 'Generate'     ]); // complete
+Route::get('/{any}',            [ API::class, 'Invalid'      ])->where([ 'any' => '.*' ]); // complete
